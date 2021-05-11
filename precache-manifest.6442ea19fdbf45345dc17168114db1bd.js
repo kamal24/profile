2b58ea99e16fe6c973caac15276af9e71b04ab93
kamalkumar <kmlgrg2425@gmail.com>
2021-05-11 17:12:13 +0530

    Updates

---
+++ precache-manifest.6442ea19fdbf45345dc17168114db1bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b687ee1a772a4b3b8df6",
+    "url": "/profile/static/css/main.404ac408.chunk.css"
+  },
+  {
+    "revision": "b687ee1a772a4b3b8df6",
+    "url": "/profile/static/js/main.b687ee1a.chunk.js"
+  },
+  {
+    "revision": "683635f2944388181fac",
+    "url": "/profile/static/css/1.ee718e7a.chunk.css"
+  },
+  {
+    "revision": "683635f2944388181fac",
+    "url": "/profile/static/js/1.683635f2.chunk.js"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fedc8db11be169c816b1a12de338e85b",
+    "url": "/profile/index.html"
+  }
+];
